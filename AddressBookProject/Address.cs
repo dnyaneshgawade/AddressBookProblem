cef4ac3cae317f31001ec0323ee6d51e61ee307a
dnyana <dnyaneshgawade903@gmail.com>
2021-07-29 18:51:28 +0530

    [Dnyanesh] add : create a contact in addressbook

--- AddressBookProject/Address.cs
+++ AddressBookProject/Address.cs
@@ -6,5 +6,18 @@ namespace AddressBookProject
 {
     class Address
     {
+        public void Add()
+        {
+            AddressBook addressbook = new AddressBook();
+            Console.WriteLine("enter Firstname,LastName,Adress,State,City,Zip,Phonenumber,Mail");
+            addressbook.FirstName = Console.ReadLine();
+            addressbook.LastName = Console.ReadLine();
+            addressbook.Address = Console.ReadLine();
+            addressbook.State = Console.ReadLine();
+            addressbook.City = Console.ReadLine();
+            addressbook.Zip = Convert.ToDouble(Console.ReadLine());
+            addressbook.PhoneNumber = Convert.ToDouble(Console.ReadLine());
+            addressbook.Email = Console.ReadLine();
+        }
     }
 }
